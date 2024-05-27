2c8198d0ae49f8050f27c1bf516e6b3f39ce30f9
Priyanshu Gupta <priyanshugupta@Priyanshus-MacBook-Air.local>
2024-05-27 23:23:33 +0530

    Add migration script to change docSize to size in Doc model

---
+++ migrationScript.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import connectDB from './lib/connectDb';
+import Doc from './model/Doc';
+
+async function migrateToSizeInsteadOfDocSize() {
+  await connectDB();
+  
+  const docs = await Doc.find({ docSize: { $exists: true } });
+
+  for (const doc of docs) {
+    doc.size = doc.docSize;
+    await doc.save();
+  }
+
+  console.log('Migration completed');
+  mongoose.connection.close();
+}
+
+migrateToSizeInsteadOfDocSize().catch(err => {
+  console.error('Migration failed:', err);
+  mongoose.connection.close();
+});
